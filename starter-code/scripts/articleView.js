f33c9e857b853be0b3f98a8fe0c7d7d67fada33a
William Madden <madden.william.d@gmail.com>
2016-12-15 12:18:41 -0800

    reolving any issues with stageing/commits before continuing to progress towards TODO goals on this branch, Thursady (now day)

--- starter-code/scripts/articleView.js
+++ starter-code/scripts/articleView.js
@@ -78,13 +78,13 @@ articleView.handleCategoryFilter = function() {
 
 articleView.handleMainNav = function () {
   $('.main-nav').on('click', '.tab', function() {
+    $(this).hide();
+    // $('.tab').hide();
     /* TODO:
       1. Hide all of the .tab-content sections
       2. Fade in the single .tab-content section that is
         associated with the .tab element's data-content attribute.
     */
-    $('.tab').hide();
-
   });
   $('.main-nav .tab:first').click();
 };
