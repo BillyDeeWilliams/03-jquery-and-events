fd6efc9f08e142f193a65fdeaf09c0298b450dd4
William Madden <madden.william.d@gmail.com>
2016-12-14 19:56:27 -0800

    I had cloned the repo down from the class github without forking. I have now forked and coppied my modified started code files from my first clone over to my new clone of a fork, efectivley transfering all modifications I made in the first to this one. However I was unable to deliniate between critical files used by git to also copie over the comit record, which is unforutunate as I was rather proud of the commit messages and effects.

--- starter-code/scripts/articleView.js
+++ starter-code/scripts/articleView.js
@@ -18,30 +18,62 @@ articleView.populateFilters = function() {
 
 articleView.handleAuthorFilter = function() {
   $('#author-filter').on('change', function() {
-  if ($(this).val()) {
-      /* TODO: If the slect box changes to an option that has a value, we should:
-          1. Hide all of the articles
-          2. Fade in only the articles that match based on on the author
-            that was aselected. Hint: use an attribute selector to find
-            those articles that match the value, and then fade them in.
-        */
-  } else {
-    /* Otherwise, we should:
-        1. Show all the articles except the template */
-  }
+    if ($(this).val()) {
+      $('article').hide(); //Hide all of the articles
+
+      $('article').not('.template').each(function(){
+        var author = $(this).find('address a').text();
+        var selection = $('#author-filter').val();
+
+        // console.log('author: ' + author);
+        // console.log($('#author-filter').val());
+        // console.log('selection: ' + selection);
+        if( author === selection ){
+          $(this).fadeIn(3000);
+        };
+      });
+    }
+    else {
+      $('article').not('.template').fadeIn(3000);
+    }
     $('#category-filter').val('');
-  });
+  }
+);
 };
 
 articleView.handleCategoryFilter = function() {
   /* TODO: Just like we do for #author-filter above, we should also handle
   change events on the #category-filter element. Be sure to reset the
   #author-filter while you're at it! */
+  $('#category-filter').on('change', function() {
+    if ($(this).val()) {
+      $('article').hide(); //Hide all of the articles
+
+      $('article').not('.template').each(function(){
+        var category = $(this).attr('data-category');
+        var selection = $('#category-filter').val();
+
+        // console.log('category: ' + category);
+        // console.log($('#category-filter').val());
+        // console.log('selection: ' + selection);
+        if( category === selection ){
+          $(this).fadeIn(3000);
+        };
+      });
+    }
+    else {
+      $('article').not('.template').fadeIn(3000);
+    }
+    $('#author-filter').val('');
+  }
+);
 };
 
 articleView.handleMainNav = function () {
   $('.main-nav').on('click', '.tab', function() {
+    $('.tab').hide();
     /* TODO:
+
       1. Hide all of the .tab-content sections
       2. Fade in the single .tab-content section that is
         associated with the .tab element's data-content attribute.
@@ -63,3 +95,7 @@ articleView.setTeasers = function() {
 };
 
 // TODO: Invoke all of the above functions (I mean, methods!):
+articleView.populateFilters();
+articleView.handleAuthorFilter();
+articleView.handleCategoryFilter();
+articleView.handleMainNav();
